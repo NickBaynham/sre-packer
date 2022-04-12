845dca83a8d744e8f34d5bd9cc78f299721f0caf
Nick Baynham <nickbaynham@gmail.com>
2022-04-12 12:29:56 -0400

    Update setup.sh
    updated to java 11

--- jenkins/standalone/setup.sh
+++ jenkins/standalone/setup.sh
@@ -2,7 +2,7 @@
 
 echo "Install Jenkins stable release"
 yum remove -y java
-yum install -y java-1.8.0-openjdk
+o yum install -y java-11-openjdk
 wget -O /etc/yum.repos.d/jenkins.repo http://pkg.jenkins-ci.org/redhat-stable/jenkins.repo
 rpm --import http://pkg.jenkins.io/redhat-stable/jenkins.io.key
 yum install -y jenkins

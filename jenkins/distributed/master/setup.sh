5b2140c567255da90432d2eb42fbe47b982e0e47
Nick Baynham <nickbaynham@gmail.com>
2022-04-12 14:54:22 -0400

    Update setup.sh

--- jenkins/distributed/master/setup.sh
+++ jenkins/distributed/master/setup.sh
@@ -5,7 +5,7 @@ amazon-linux-extras install epel -y
 
 echo "Install Jenkins stable release"
 yum remove -y java
-yum install -y java-1.8.0-openjdk
+yum install -y java-11-openjdk
 wget -O /etc/yum.repos.d/jenkins.repo http://pkg.jenkins-ci.org/redhat-stable/jenkins.repo
 rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io.key
 yum install -y jenkins
